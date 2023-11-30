a02d0c7fcfe60c78760d0042ff65a90d351bb214
AditiR_42 <aditisanram@gmail.com>
2023-11-30 02:10:49 -0500

    modify plo

--- examples/ZeusDataLoader/cifar100/train_cifar.py
+++ examples/ZeusDataLoader/cifar100/train_cifar.py
@@ -151,8 +151,8 @@ def main(args: argparse.Namespace) -> None:
         optimum_selector=MaxSlowdownConstraint(
             factor=get_env("ZEUS_MAX_SLOWDOWN", float, 1.1),
         ),
-        warmup_steps=10,
-        profile_steps=40,
+        warmup_steps=5,
+        profile_steps=15,
         pl_step=35,
         profile_path="trace.json"
     )
